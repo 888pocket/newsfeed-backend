2d27c626f704329c8604c001d118687a1b2e9c4e
888pocket <dndmdo8@gmail.com>
2024-01-24 15:34:24 +0900

    init

---
+++ src/test/java/com/joosangah/ordersystem/OrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joosangah.ordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
