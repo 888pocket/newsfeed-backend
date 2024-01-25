e75f0db1869b0dbddb01cd4d1ea52bc13bd6a9d5
888pocket <dndmdo8@gmail.com>
2024-01-25 20:42:57 +0900

    build: jpa 설정

--- src/main/java/com/joosangah/ordersystem/OrderSystemApplication.java
+++ src/main/java/com/joosangah/ordersystem/OrderSystemApplication.java
@@ -2,12 +2,12 @@ package com.joosangah.ordersystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class OrderSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderSystemApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSystemApplication.class, args);
+    }
 }
