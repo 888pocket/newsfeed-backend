36e7102ffa31fda5b24be9ea0e1a9a0ef6f3109d
888pocket <dndmdo8@gmail.com>
2024-01-30 13:40:54 +0900

    build: build.gradle 중복 제거

--- src/main/java/com/joosangah/ordersystem/post/service/PostService.java
+++ src/main/java/com/joosangah/ordersystem/post/service/PostService.java
@@ -1,11 +1,14 @@
 package com.joosangah.ordersystem.post.service;
 
+import com.joosangah.ordersystem.interaction.domain.entity.Interaction;
+import com.joosangah.ordersystem.interaction.repository.InteractionRepository;
 import com.joosangah.ordersystem.post.domain.dto.request.PostForm;
 import com.joosangah.ordersystem.post.domain.dto.response.PostResponse;
 import com.joosangah.ordersystem.post.domain.entity.Post;
 import com.joosangah.ordersystem.post.mapper.PostResponseMapper;
 import com.joosangah.ordersystem.post.repository.PostRepository;
 import com.joosangah.ordersystem.user.domain.entity.User;
+import com.joosangah.ordersystem.user.domain.enums.InteractionType;
 import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -25,6 +28,7 @@ public class PostService {
     private final PostResponseMapper postResponseMapper;
 
     private final PostRepository postRepository;
+    private final InteractionRepository interactionRepository;
 
     public Post loadPost(String postId) {
         return postRepository.findByIdAndDeletedIsFalse(postId)
@@ -36,13 +40,27 @@ public class PostService {
     }
 
     @Transactional
+    public Post savePost(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Transactional
     public String addPost(User user, PostForm postForm) {
         Post newPost = Post.builder()
                 .user(user)
+                .title(postForm.getTitle())
                 .content(postForm.getContent())
                 .deleted(false).build();
 
         postRepository.save(newPost);
+
+        interactionRepository.save(Interaction.builder()
+                .userId(user.getId())
+                .targetId(newPost.getId())
+                .type(InteractionType.POST)
+                .description(
+                        String.format("%s님이 %s 포스트를 작성했습니다.", user.getName(), newPost.getTitle()))
+                .build());
 
         return newPost.getId();
     }
