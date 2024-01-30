79fb893ab10b1fedb31bb6635f088b64a8f73a67
888pocket <dndmdo8@gmail.com>
2024-01-30 13:45:30 +0900

    refactor: AuditEntity 추가

--- src/main/java/com/joosangah/ordersystem/interaction/domain/entity/Interaction.java
+++ src/main/java/com/joosangah/ordersystem/interaction/domain/entity/Interaction.java
@@ -1,24 +1,18 @@
 package com.joosangah.ordersystem.interaction.domain.entity;
 
+import com.joosangah.ordersystem.common.domain.AuditEntity;
 import com.joosangah.ordersystem.user.domain.enums.InteractionType;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import java.time.LocalDateTime;
 import lombok.Builder;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.CompoundIndex;
-import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(collection = "interactions")
-@CompoundIndexes({
-        @CompoundIndex(name = "compound_key_index", def = "{'userId' : 1, 'targetId' : 1, 'type' : 1}", unique = true)
-})
-public class Interaction {
+public class Interaction extends AuditEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -30,13 +24,14 @@ public class Interaction {
     private String targetId;
     @Enumerated(EnumType.STRING)
     private InteractionType type;
-    @CreatedDate
-    private LocalDateTime createdAt;
+
+    private String description;
 
     @Builder
-    public Interaction(String userId, String targetId, InteractionType type) {
+    public Interaction(String userId, String targetId, InteractionType type, String description) {
         this.userId = userId;
         this.targetId = targetId;
         this.type = type;
+        this.description = description;
     }
 }
--- src/main/java/com/joosangah/ordersystem/post/domain/entity/Post.java
+++ src/main/java/com/joosangah/ordersystem/post/domain/entity/Post.java
@@ -1,71 +1,45 @@
 package com.joosangah.ordersystem.post.domain.entity;
 
 import com.joosangah.ordersystem.comment.domain.entity.Comment;
+import com.joosangah.ordersystem.common.domain.AuditEntity;
 import com.joosangah.ordersystem.user.domain.entity.User;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Builder;
-import org.springframework.data.annotation.CreatedDate;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(collection = "posts")
-public class Post {
+@Getter
+public class Post extends AuditEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
     @DBRef
     private User user;
+    private String title;
     private String content;
     @Field("is_deleted")
     private boolean deleted;
-    @Field("created_at")
-    @CreatedDate
-    private LocalDateTime createdAt;
-    @Field("updated_at")
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 
     @DBRef
     private List<Comment> commentList;
+    @DBRef
+    private List<User> likeUserList;
 
     @Builder
-    public Post(User user, String content, boolean deleted) {
+    public Post(User user, String title, String content, boolean deleted) {
         this.user = user;
+        this.title = title;
         this.content = content;
         this.deleted = deleted;
         this.commentList = new ArrayList<>();
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public List<Comment> getCommentList() {
-        return commentList;
-    }
-
-    public void setCommentList(
-            List<Comment> commentList) {
-        this.commentList = commentList;
+        this.likeUserList = new ArrayList<>();
     }
 }
