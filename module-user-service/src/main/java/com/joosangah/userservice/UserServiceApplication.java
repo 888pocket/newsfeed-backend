bedbd36342219b7bb0d118dcbf747185d6403f8e
888pocket <dndmdo8@gmail.com>
2024-02-08 12:22:53 +0900

    feat: eureka 서버에 서비스 등록

--- module-user-service/src/main/java/com/joosangah/userservice/UserServiceApplication.java
+++ module-user-service/src/main/java/com/joosangah/userservice/UserServiceApplication.java
@@ -3,12 +3,14 @@ package com.joosangah.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableMongoAuditing
 @EnableFeignClients
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
